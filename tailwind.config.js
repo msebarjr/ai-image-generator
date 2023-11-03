b0a1b6a1b2cefeedd295a32a391ce41bdb0938f9
msebarjr <msebarjr@gmail.com>
2023-11-02 20:25:25 -0400

    Created header and logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,12 +8,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        squarePeg: ["'Square Peg', cursive"],
       },
     },
   },
   plugins: [],
-}
+};
